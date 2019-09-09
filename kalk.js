47503dc0d74e68706dc9a8f9d13f839e4609d4e7
Ruslan Sultangareev <rs.94@mail.ru>
2019-09-09 18:22:56 +0500

    Во время прохождения курса, делал задания
    Учимся js

---
+++ kalk.js
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+    <script type="text/javascript">
+    alert("Самое время посчитать длину трапеции!");
+var basisA = prompt("Введите длину основания a:");
+var basisB = prompt("Введите длину основания b:");
+var height = prompt("Введите высоту трапеции:");
+var resultArea = ((parseInt(basisA) + parseInt(basisB)) / 2) * parseInt(height);
+alert("Площадь трапеции будет равна " + resultArea);
+</script>
